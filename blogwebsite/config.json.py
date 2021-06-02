96c4c820dbaf4e2237ca484a05abecb2b5150254
amitsinghbora <amit06s03bora94@gmail.com>
2021-06-02 23:54:33 +0530

    Project started

---
+++ blogwebsite/config.json.py
@@ -0,0 +1,26 @@
+{
+    "params":
+    {"local_server":"True",
+     "local_uri":"mysql://root:@localhost/explore",
+     "prod_uri":"mysql://root:@localhost/explore",
+     "fb_url":"https://facebook.com/explore",
+     "tw_url":"https://twitter.com/explore",
+     "gh_url":"https://github.com/explore",
+     "blog_name": "MyBook",
+     "gmail_user": "amit06bora1994@gmail.com",
+     "gmail_password": "###########",
+     "about_text": "hello my name is amit and this is my about page",
+     "num_of_posts": 3,
+     "login_image": "login.jpg",
+     "admin_user": "amit",
+     "admin_password": "amitsingh",
+     "upload_location":"C:\\Users\\Dell\\PycharmProjects\\new"
+
+
+
+    }
+
+
+
+
+}
